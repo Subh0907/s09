1c1d37334fd5a513c6ed83ed93114b53253e3e6e
Subh0907 <shubhamdotsinghh@gmail.com>
2025-06-14 12:00:30 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+       
+
+        System.out.print("Enter first number: ");
+        double num1 = 36;
+
+        System.out.print("Enter second number: ");
+        double num2 = 87;
+
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
